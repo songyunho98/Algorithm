265e1cc31675db916071d6116fb8e98427f8cb92
songyunho98 <song502122120@gmail.com>
2021-11-19 16:29:20 +0900

    fisrt commit

--- programmers/lessons64061.js
+++ programmers/lessons64061.js
@@ -2,8 +2,24 @@
 
 function solution(board, moves) {
     var answer = 0;
+    const result = [];
 
-
+    for(var i=0;i<moves.length;i++){
+        for(var j=0;j<board.length;j++){
+            if(board[j][moves[i]-1] !== 0){ 
+                if(result[result.length-1]===board[j][moves[i]-1]){ 
+                    result.pop();
+                    answer+=2;
+                    board[j][moves[i]-1]=0;
+                    break;
+                }else{  
+                    result.push(board[j][moves[i]-1]);
+                    board[j][moves[i]-1]=0;
+                    break;
+                }
+            }
+        }
+    }
     return answer;
 }
 
